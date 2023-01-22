df46cce3c096a180a6c26a218f0d6a712bce20c5
shinkar94 <romka94shinkarenko@gmail.com>
2023-01-22 11:20:27 +0300

    lesson 7 youtube samurai

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,30 @@ import './App.css';
 const  App = () =>{
   return (
     <div className="App">
-     <div>OneDiv</div>
-     <ul>
-         <li>js</li>
-         <li>css</li>
-         <li>php</li>
-         <li>react</li>
-     </ul>
+     <Header />
+     <Technologies />
     </div>
   );
+}
+const Technologies = () =>{
+    return(
+        <ul>
+            <li>js</li>
+            <li>css</li>
+            <li>php</li>
+            <li>react</li>
+        </ul>
+    )
+}
+
+const Header = () =>{
+    return (
+        <div>
+            <a href="#">Menu</a>
+            <a href="#">Find</a>
+            <a href="#">Messages</a>
+        </div>
+    )
 }
 
 export default App;
